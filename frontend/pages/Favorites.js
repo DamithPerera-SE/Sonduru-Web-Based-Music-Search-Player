0f70fe297c64080fdd26efce87b95e27794ac9ba
Sasini Kavindya <sasinikavindya19@gmail.com>
2025-12-29 18:46:01 +0530

    Update Favorites.js

---
+++ frontend/pages/Favorites.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Favorites() {
+  return (
+    <div className="container py-5 text-white">
+      <h3>❤️ Favorites</h3>
+      <p>Favorites loaded from backend</p>
+    </div>
+  );
+}
