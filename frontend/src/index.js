aada29432e5cc05d898cc5065cc74cfacf1748db
Sasini Kavindya <sasinikavindya19@gmail.com>
2025-12-29 18:45:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./Styles/themes.css";
+import "./styles/theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
